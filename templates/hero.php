b2c4034fbc0b0951a77574d4ecb3b1a1b4268cde
Qrczok69 <tburzynski.finanse@gmail.com>
2026-01-20 15:22:26 +0100

    Dodanie hero.php - sekcja powitalna

---
+++ templates/hero.php
@@ -0,0 +1,10 @@
+<section class="hero">
+    <div class="container">
+        <h1>Znajdź i sfinansuj swoje nowe auto</h1>
+        <p>Wyszukiwarka tysięcy modeli, leasing i najem w jednym miejscu.</p>
+        <div style="margin-top: 30px;">
+            <a href="#baza-aut" class="btn btn-primary" style="margin-right: 15px;">Przeglądaj Auta</a>
+            <a href="#kalkulator" class="btn btn-outline">Oblicz Ratę</a>
+        </div>
+    </div>
+</section>
