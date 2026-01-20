fe848febb7b4b31f88222273b43d4904b3538a72
Qrczok69 <tburzynski.finanse@gmail.com>
2026-01-20 15:23:38 +0100

    Add vehis-search.php template for vehicle search

---
+++ templates/vehis-search.php
@@ -0,0 +1,17 @@
+<section id="baza-aut" class="section-padding vehis-section">
+    <div class="container">
+        <div class="text-center">
+            <h2 style="color: <?= COLOR_NAVY ?>; font-size: 32px;">Baza dostępnych aut</h2>
+            <p style="color: #666; margin-bottom: 20px;">Wybierz samochód, a my zajmiemy się resztą.</p>
+        </div>
+
+        <div class="vehis-container">
+            <script type="text/javascript">
+                !function (v, e, h, i, s) {var t=null,m="https://embed.vehis.pl/?embed_client_token=c4fb53f0-0201-4a79-a51e-d9a225830b5d";
+                    v.write("<ifr"+"ame id=\""+i+"\"></ifr"+"ame>"),v.write("<scr"+"ipt src=\""+m+"/js/embed.js\" type=\"text/javascript\"></scr"+"ipt>");
+                }(document, window, null, 'v-embed', null);
+            </script>
+        </div>
+        
+    </div>
+</section>
