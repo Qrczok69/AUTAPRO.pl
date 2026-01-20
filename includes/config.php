df01a86142bd27ad5e3933ef1f7b1eb0c2ab35f9
Qrczok69 <tburzynski.finanse@gmail.com>
2026-01-20 15:17:10 +0100

    Add configuration file for site settings

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+// includes/config.php
+
+// === ZMIEŃ PONIŻEJ NA SWÓJ ADRES EMAIL ===
+define('SITE_EMAIL', 'twoj-email@domena.pl'); 
+// ==========================================
+
+define('SITE_NAME', 'AutaPro.pl');
+define('SITE_PHONE', '+48 600 700 800');
+
+define('COLOR_NAVY', '#1D3150');
+define('COLOR_TEAL', '#2AAAB0');
+?>
